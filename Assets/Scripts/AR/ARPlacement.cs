16e9cd166c9c7f1a135c5662684c9a0e617b036c
Shadow229 <55843220+Shadow229@users.noreply.github.com>
2020-02-20 11:10:22 +0000

    added raycast manager

--- Assets/Scripts/AR/ARPlacement.cs
+++ Assets/Scripts/AR/ARPlacement.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.Experimental.XR;
-//using UnityEngine.XR.ARSubsystems;
+//using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
 using System;
 
 public class ARPlacement : MonoBehaviour
@@ -13,12 +13,8 @@ public class ARPlacement : MonoBehaviour
     private ARSessionOrigin arOrigin;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
+    public ARRaycastManager arRaycastManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        arOrigin = FindObjectOfType<ARSessionOrigin>();
-    }
 
     // Update is called once per frame
     void Update()
@@ -44,7 +40,7 @@ public class ARPlacement : MonoBehaviour
     {
         Vector3 screenCentre = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0f));
         var hits = new List<ARRaycastHit>();
-        arOrigin.GetComponent<ARRaycastManager>().Raycast(screenCentre, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+        arRaycastManager.Raycast(screenCentre, hits, TrackableType.Planes);
 
         placementPoseIsValid = hits.Count > 0;
 
